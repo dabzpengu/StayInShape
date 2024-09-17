e16b8074e6977f78594b6933d32712f50bda7b92
OWEE\trlfr <tr.lfrostbyte@gmail.com>
2024-09-18 01:22:58 +0800

    Add Step Counter functionality to Resource Collection Scene

--- Assets/Scripts/SOs/SaveDataSOs/SaveManagerSO.cs
+++ Assets/Scripts/SOs/SaveDataSOs/SaveManagerSO.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Save Manager", menuName = "Saving/Save Manager SO")]
