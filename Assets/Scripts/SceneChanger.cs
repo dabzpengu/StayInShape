1c76bd82b0cd2bc5518d183059958ce6575b2fa2
OWEE\trlfr <tr.lfrostbyte@gmail.com>
2024-10-24 00:30:42 +0800

    Improve scene changer to destroy AR assets for cleanliness

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARCore;
+using UnityEngine.XR.ARFoundation;
 
 // Standard scene changing function
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] ARSession arSession;
+    [SerializeField] ARTrackedImageManager trackedImageManager;
+
     public void GoToScene(string sceneName)
     {
+        if (arSession != null && trackedImageManager != null)
+        {
+            arSession.Reset();
+            trackedImageManager.enabled = false;
+            foreach (var trackedImage in trackedImageManager.trackables)
+            {
+                Destroy(trackedImage.gameObject);
+                Debug.Log("Destroy!");
+            }
+        }
         SceneManager.LoadScene(sceneName);
     }
 }
